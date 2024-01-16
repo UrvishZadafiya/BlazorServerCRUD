8c5644defc4ecf172b8b821f880aa63442a4298c
Urvish Zadafiya <urvishzadafiya@gmail.com>
2024-01-16 08:57:16 +0530

    Init for git

---
+++ Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorServerCRUD.Models;
+
+namespace BlazorServerCRUD.Services
+{
+    public interface IPersonService
+    {
+        bool AddUpdate(Person person);
+
+        bool Delete(int id);
+
+        Person FindById(int id);
+
+        List<Person> GetAll();
+    }
+}
